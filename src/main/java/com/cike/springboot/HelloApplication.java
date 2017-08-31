2fac29918f1a06646cd950d028c50b213aa53918
cike <875569847@qq.com>
2017-08-31 22:57:35 +0800

    springboot学习

--- src/main/java/com/cike/springboot/HelloApplication.java
+++ src/main/java/com/cike/springboot/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.cike.springboot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -15,10 +16,13 @@ public class HelloApplication {
     @RequestMapping("hello")
     @ResponseBody
     public String hello() {
-        return "hello world！";
+        return "hello world！刺客";
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloApplication.class, args);
+//      SpringApplication.run(HelloApplication.class, args);
+        SpringApplication application = new SpringApplication(HelloApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
     }
 }
